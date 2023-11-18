b64aa8bfce687914d326d7fbc9b730ceed40bfff
jjzapata99 <jjzapata99@espol.edu.ec>
2023-11-18 11:29:03 -0500

    Changes

--- routes/categories.js
+++ routes/categories.js
@@ -33,7 +33,40 @@ router.get('/', function (req, res, next) {
       .catch(error => res.status(400).send(error))
   
   });
-
+  router.put('/:id', function (req, res, next) {
+    let id = parseInt(req.params.id);
+  
+    Category.update(req.body, {
+      where: {
+        CategoryID: id
+      }
+    })
+      .then(result => {
+        if (result[0] === 1) {
+          res.json({ message: 'Categoría actualizada correctamente' });
+        } else {
+          res.status(404).json({ message: 'Categoría no encontrada' });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+  });
+  router.delete('/:id', function (req, res, next) {
+    let id = parseInt(req.params.id);
+  
+    Category.destroy({
+      where: {
+        CategoryID: id
+      }
+    })
+      .then(result => {
+        if (result === 1) {
+          res.json({ message: 'Categoría eliminada correctamente' });
+        } else {
+          res.status(404).json({ message: 'Categoría no encontrada' });
+        }
+      })
+      .catch(error => res.status(400).send(error));
+  });  
   router.post('/', function (req, res, next) {
 
     let data = req.body;
